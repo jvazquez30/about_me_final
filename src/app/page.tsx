a5637e61f869523b9232e7a1491d0ffc8b14025a
jvazquez30 <josevazquez881@gmail.com>
2024-12-08 19:59:09 -0800

    changed react version 18.3, added react typewriter.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,11 +34,14 @@ export default function Home() {
         <h1 className="font-extrabold text-4xl">Hello, I am  
         </h1>
         <h2 className="font-extrabold text-7xl">Jose</h2>
-        <h3 className="font-extrabold text-2xl ">A tech enthusiast </h3>
+        <h3 className="font-extrabold text-2xl ">A tech enthusiast! </h3>
         <p className="font-extrabold py-2 ">I&apos;m a passionate Full-Stack Developer dedicated to delivering exceptional results from Front-End to Back-End, with a strong work ethic and a love for learning the latest technologies. 
           My current stack includes JavaScript, React, HTML/CSS, Node.js, and Tailwind CSS, with expertise in quality assurance through unit and integration testing. 
           Always eager to expand my network and explore new opportunities, I welcome the chance to connect and collaborate!</p>
         </div>
+
+
+
         
         
         </div>

4c9cf9a7d36db9b7202070098fa3d815d22ab9cb
jvazquez30 <josevazquez881@gmail.com>
2024-12-09 01:52:39 -0800

    added message to top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
+'use client'
 import React from "react";
 import Image from "next/image";
+import { Typewriter } from "react-simple-typewriter";
 import { TbSquareRoundedLetterJFilled } from "react-icons/tb";
 
 
 export default function Home() {
+
   return (
     <div className="">
       <header className="p-8 flex justify-between border-b border-lg border-gray-500">
         <a className="flex items-center font-semibold ">
-          <TbSquareRoundedLetterJFilled className="text-4xl"/>
+          <TbSquareRoundedLetterJFilled className="text-4xl" />
         </a>
 
         <ul className="flex ">
@@ -21,29 +24,42 @@ export default function Home() {
 
 
       <main className="p-8 w-full">
-        <div className="flex">
-        <Image 
-        alt="" 
-        src={"/Futuristic_Profile_Pic.png"} 
-        width={300} 
-        height={300}
-        className="border rounded-full mr-2"
-        />
-
-        <div className="ml-3">
-        <h1 className="font-extrabold text-4xl">Hello, I am  
-        </h1>
-        <h2 className="font-extrabold text-7xl">Jose</h2>
-        <h3 className="font-extrabold text-2xl ">A tech enthusiast! </h3>
-        <p className="font-extrabold py-2 ">I&apos;m a passionate Full-Stack Developer dedicated to delivering exceptional results from Front-End to Back-End, with a strong work ethic and a love for learning the latest technologies. 
-          My current stack includes JavaScript, React, HTML/CSS, Node.js, and Tailwind CSS, with expertise in quality assurance through unit and integration testing. 
-          Always eager to expand my network and explore new opportunities, I welcome the chance to connect and collaborate!</p>
+        <div className="w-full text-center">
+        <h1 className="font-extrabold text-4xl">Hello, {""}
+              <Typewriter
+                words={["Friend!", "Collegue!", "World!"]}
+                loop={6}
+                cursor
+                cursorStyle="|"
+                typeSpeed={100}
+                deleteSpeed={150}
+                delaySpeed={1000}
+              />
+            </h1>
         </div>
 
 
 
-        
-        
+
+        <div className="flex">
+          <Image
+            alt=""
+            src={"/Futuristic_Profile_Pic.png"}
+            width={300}
+            height={300}
+            className="border rounded-full mr-2"
+          />
+
+          <div className="ml-3">
+            <p className="font-extrabold py-2 ">I&apos;m a passionate Full-Stack Developer dedicated to delivering exceptional results from Front-End to Back-End, with a strong work ethic and a love for learning the latest technologies.
+              My current stack includes JavaScript, React, HTML/CSS, Node.js, and Tailwind CSS, with expertise in quality assurance through unit and integration testing.
+              Always eager to expand my network and explore new opportunities, I welcome the chance to connect and collaborate!</p>
+          </div>
+
+
+
+
+
         </div>
 
 
